be2e933b3f359f851cc646341a22a162b579eb58
Aperaine <Aperaine@users.noreply.github.com>
2025-07-17 15:19:35 +0900

    Collision, Attempted projectile release

--- riggedBasketball/scripts/ball.gd
+++ riggedBasketball/scripts/ball.gd
@@ -7,21 +7,36 @@ extends RigidBody2D
 var arcHeight:int = 200
 var syncProjectile:bool = false
 
-
+@onready var frozenposition = position
 func _physics_process(delta: float) -> void:
 	## for testing
 	if Input.is_action_just_pressed("moveRight"):
 		syncProjectile = true
+		projectile.move()
 		projectile.launch(originalPos, target, arcHeight, speed)
 	
-	if syncProjectile:
-		projectile._physics_process(delta)
-		
-		position = projectile.position
-		rotation = projectile.rotation
+	if Input.is_action_just_pressed("moveLeft"):
+		syncProjectile = false
+		set_physics_process(true)
 	
+	if syncProjectile:
+		freeze = true
+		#position = projectile.position
+		#rotation = projectile.rotation
+		position = Vector2(0,0)
+		print("setting position")
+		var frozenposition = position 
+		print(frozenposition)
+	else:
+		print(frozenposition)
+		freeze = false
+		if frozenposition != originalPos:
+			position = frozenposition
+		
+		print("not setting position")
 
 
 func _on_body_entered(_body: Node) -> void:
-	print("enter")
+	#print("enter")
 	syncProjectile = false
+	projectile.stop()
