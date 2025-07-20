db8fa979e01ea4942e026960d39e47009c384c0d
Aperaine <Aperaine@users.noreply.github.com>
2025-07-20 18:00:18 +0900

    Updated ball release + Ball Boundaries
    - Now ball release based on downward velocity
    - Target boundaries for the thrower have been set

--- riggedBasketball/scenes/thrower.gd
+++ riggedBasketball/scenes/thrower.gd
@@ -1,1 +1,4 @@
 extends Node2D
+
+@export var targetBoundaryL:Vector2 = Vector2(30,100)
+@export var targetBoundaryR:Vector2 = Vector2(610,150)
--- riggedBasketball/scripts/ball.gd
+++ riggedBasketball/scripts/ball.gd
@@ -9,6 +9,9 @@ var syncProjectile:bool = false
 
 var firstTimer: bool = true
 
+var yBeforePos:float
+var yAfterPos:float
+
 func _ready() -> void:
 	freeze = true
 	speed = round(abs(originalPos.x-target.x)/220 + 2)
@@ -23,15 +26,14 @@ func _physics_process(_delta: float) -> void:
 	if syncProjectile:
 		projectileSync()
 		
-		if global_position.y < originalPos.y-arcHeight+40:
-			if firstTimer:
-				firstTimer = false
-				get_tree().create_timer(0.4).timeout.connect(physicsRelease)
+		if yBeforePos < yAfterPos:
+			physicsRelease()
 
 func projectileSync() -> void:
+	yBeforePos = global_position.y
 	global_position = projectile.position
 	global_rotation = projectile.rotation
-	
+	yAfterPos = global_position.y
 
 func physicsRelease():
 	$CollisionShape2D.disabled = false
@@ -39,7 +41,6 @@ func physicsRelease():
 	projectile.stop()
 	freeze = false
 	
-	linear_velocity.y = 300
-	linear_velocity.x = target.x - originalPos.x
+	linear_velocity.x = (target.x - originalPos.x)/1.4
 	
 	set_physics_process(false)
