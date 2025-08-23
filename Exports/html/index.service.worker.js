1d1adcaef13ddafa4f527d636f3ffd726b957655
Aperaine <Aperaine@users.noreply.github.com>
2025-08-23 22:10:09 +0900

    Fixed the android input

--- Exports/html/index.service.worker.js
+++ Exports/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1754977967|185282308';
+const CACHE_VERSION = '1755954554|19358434366';
 /** @type {string} */
 const CACHE_PREFIX = 'Rigged Basketbal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
