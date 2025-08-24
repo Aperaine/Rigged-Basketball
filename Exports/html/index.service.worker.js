451dd9c896e447aa47a54b7c42a9344608fd43f5
Aperaine <Aperaine@users.noreply.github.com>
2025-08-24 18:45:50 +0900

    Fixed gameover focus

--- Exports/html/index.service.worker.js
+++ Exports/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755957024|247093066';
+const CACHE_VERSION = '1756028716|143760068';
 /** @type {string} */
 const CACHE_PREFIX = 'Rigged Basketbal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
