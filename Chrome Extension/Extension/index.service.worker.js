b03b6fdc78808e699ad16ae9310dcf94dd4e50e9
Aperaine <Aperaine@users.noreply.github.com>
2025-09-20 21:56:18 +0900

    Updated extension

--- Chrome Extension/Extension/index.service.worker.js
+++ Chrome Extension/Extension/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1757497884|147617702';
+const CACHE_VERSION = '1758372683|437897417';
 /** @type {string} */
 const CACHE_PREFIX = 'Rigged Basketbal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
